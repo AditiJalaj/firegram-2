bcd461d3910b69f1ea07b8b47ce84be76bc1d635
ajalaj <aditi.jalaj@gmail.com>
2021-06-02 11:48:59 +0530

    till setprogress to 0 once we get url

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -6,45 +6,58 @@ const UploadForm = () => {
     const [file,setFile]=useState('')
     const [progress,setProgress]=useState('')
     const [err,setErr]=useState('')
+    const [url,setUrl]=useState(null)
+
+    const types=["image/png" ,"image/jpeg"];
 
     const changeHandler=(e)=>{
         var f=e.target.files[0]
-        setFile(f) 
+        if(f && types.includes(f.type)){
+            setFile(f)
+            setErr('')
+        }
+        else{
+            setFile('')
+            setErr('Please select a png/jpg file only.')
+        }
      }
 
-       
-        useEffect(()=>{
+    useEffect(()=>{
             const storageRef=storage.ref(`images/${file.name}`)
             storageRef.put(file).on('state_changed',(snapshot)=>{
                 let percent=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
                 setProgress(percent)
-               
                 console.log(`percent is ${percent}`)
+            },(error)=>{
+                console.log(err)
+                setErr(error)
+            },
+            ()=>{
+                storageRef.getDownloadURL()
+                .then((u)=>{
+                    console.log('url is ',u)
+                    setUrl(u)
+                })
             })
-        },[file])
+     },[file])
 
 
-        // // DOUBT-  too many renders when i keep progress as dependency
-        // useEffect(()=>{
-        //     const storageRef=storage.ref(`images/${file.name}`)
-        //     storageRef.put(file).on('state_changed',(snapshot)=>{
-        //         let percent=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
-        //         setProgress(percent)
-        //         console.log('render')
-        //         console.log(`percent is ${percent}`)
-        //     })
-
-        // },[file,progress])
+        //useeffect to run to setprogress to 0 once we have url of file
+        useEffect(()=>{
+            if(url){
+                setProgress(0)
+            }
+        },[url])
 
     console.log(file)
    
     console.log(file)
     return (<>
         <input type='file' onChange={changeHandler}/>
-
-        {file &&  <img src={file} 
-            alt="uploaded"/>}
-        {err}
+        {file &&<div> {file.name}</div>}
+        {err && <div> {err} </div>}
+        {file && <div style={{width:progress+"%"}} className='progress-bar'> </div>}
+        {url && <div> url is {url} </div>}
         </> );
 }
  
