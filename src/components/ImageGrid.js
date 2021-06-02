f5f255959b19e5594066a08f3d8a4664a9bb2c89
ajalaj <aditi.jalaj@gmail.com>
2021-06-02 22:05:01 +0530

    fixed the prev issue

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -8,9 +8,9 @@ const ImageGrid = () => {
 
     useEffect(()=>{
 
-        db.collection('images')
+       const unsub= db.collection('images')
         .orderBy('createdAt','desc')
-        .onSnapshot((snap)=>{
+        .onSnapshot((snap)=>{  
             let documents=[]
             snap.forEach((doc)=>{
               documents.push({data:doc.data(),id:doc.id})
@@ -18,28 +18,45 @@ const ImageGrid = () => {
           setDocs(documents)
         })
 
+        return ()=>unsub()
     },['images'])
 
-    //doubt what happens if we put docs as a dependency
+    //doubt what happens if we put docs as a dependency -infinite loop
 
-    console.log('outside: docs is ',docs)
+    console.log('outside: docs.url is ',docs)
     
-    let inf=[]
+   /* REDUNDANT
+    // let inf=[]
 
-    //docs.length > 0 will fix docs.map is not a fucntion on mount
-    if(docs!=undefined && docs.length>0){
-      docs.map((i)=>{
-        console.log('i is ',i.data.url)
-        inf.push(i.data.url)
-    })}
+    // //docs.length > 0 will fix docs.map is not a fucntion on mount
+    // if(docs!=undefined && docs.length>0){
+    //   docs.map((i)=>{
+    //     console.log('i is ',i.data.url)
+    //     inf.push(i.data.url)
+    // })}
+
+    */
    
-   
+//    const deleteHandler=()=>{
+//        let dbRef=db.collection('images')
+//        dbRef.doc(y).delete().then(()=>{
+//            console.log('Deleted')
+//        }).catch((err)=>{
+//            console.log('error',err)
+//        })
+//    }
 
     return ( <>
         ImageGrid
         <div>
-        {inf && inf.map((img)=>{
-            return(<img style={{width:"100px",height:"102px"}} src={img} alt="not displaying"></img>
+        {docs && docs.map((img)=>{
+            return(<>
+                <div style={{position:"relative"}}>
+                <img style={{margin:"2px",width:"100px",height:"102px"}} key={img.id} src={img.data.url} alt="not displaying"></img>
+        
+                <button  style={{color:"red" ,position:"absolute"}}>X</button>
+                </div>
+            </>
             ) 
 
         })}
